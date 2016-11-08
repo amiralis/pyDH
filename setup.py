c986a19c3094c9af0b80c263d2da1e49586161d1
Amirali Sanatinia <amiralis@users.noreply.github.com>
2016-11-08 17:51:16 -0500

    setup pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,23 @@
 
 from setuptools import setup
 
-
-def readme():
-    with open('README.rst') as f:
-        return f.read()
+setup(
+  name = 'pyDH',
+  packages = ['pyDH'],
+  version = '0.1',
+  description = 'Pure Python Implementation of Diffie-Hellman Key Exchange',
+  author = 'Amirali Sanatinia',
+  author_email = 'amirali@ccs.neu.edu',
+  url = 'https://github.com/amiralis/pyDH',
+  download_url = 'https://github.com/amiralis/pyDH/tarball/0.1',
+  keywords = ['crypto', 'Diffie Hellman', 'Key Exchange'],
+  classifiers = [
+    	'License :: OSI Approved :: Apache Software License',
+    	'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+		'Topic :: Security :: Cryptography',
+		],
+)
