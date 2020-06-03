7cefb5f2b9a8accaeda1b3f7a483694831b95045
Amirali Sanatinia <amirali@ccs.neu.edu>
2020-06-02 20:25:47 -0400

    add unit tests

---
+++ test/test_pydh.py
@@ -0,0 +1,10 @@
+import pyDH
+
+def test_pydh_keygen():
+    d1 = pyDH.DiffieHellman()
+    d2 = pyDH.DiffieHellman()
+    d1_pubkey = d1.gen_public_key()
+    d2_pubkey = d2.gen_public_key()
+    d1_sharedkey = d1.gen_shared_key(d2_pubkey)
+    d2_sharedkey = d2.gen_shared_key(d1_pubkey)
+    assert d1_sharedkey == d2_sharedkey
